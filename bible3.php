3688720321c555797497d80b0a79d013bf0a6929
David Chang <cwjen888@gmail.com>
2018-10-01 21:27:48 -0700

    changed PHP ini memory_limit to 2048M

--- bible3.php
+++ bible3.php
@@ -1,6 +1,6 @@
 <?php
 
-ini_set('memory_limit', '1024M');
+ini_set('memory_limit', '2048M');
 
 require_once "/jieba/vendor/multi-array/MultiArray.php";
 require_once "/jieba/vendor/multi-array/Factory/MultiArrayFactory.php";
